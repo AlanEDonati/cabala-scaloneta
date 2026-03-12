0692b2043ec4e1fce822a9daf1ae26b1cd77127b
Alan Donati <alanestebandonati@gmail.com>
2026-03-12 19:09:13 -0300

    use env database url

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_9EnJlwz0msMV@ep-long-leaf-a4vimdow-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
