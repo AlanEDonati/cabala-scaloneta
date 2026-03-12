ffa8be12fc2ac7857ed39c73804190d820dda832
Alan Donati <alanestebandonati@gmail.com>
2026-03-12 11:22:56 -0300

    fix puerto para producción

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const db = require("./db");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
